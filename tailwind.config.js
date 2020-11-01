8b7775859f42f1781cd0de690a67a21c6f99acc3
Sergio Felzener <felzenerdj@gmail.com>
2020-10-31 22:16:34 -0300

    kanban update

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  future: {
+    // removeDeprecatedGapUtilities: true,
+    // purgeLayersByDefault: true,
+  },
+  purge: [],
+  theme: {
+    extend: {
+      colors: {
+        cor1: '#A833FF',
+        cor2: '#FF336E'
+      }
+    },
+  },
+  variants: {},
+  plugins: [],
+}
